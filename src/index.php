87635ce1117bd3b443de249e67aadcfef2a66556
Tyler Hanavan <tyler.hanavan@sedibyte.com>
2024-03-16 15:30:24 -0400

    begin url parsing

--- src/index.php
+++ src/index.php
@@ -1,5 +1,10 @@
-<?php
+<?php declare(strict_types=1); // strict typing
 
+    $request_method = $_SERVER['REQUEST_METHOD']; // GET, POST, PUT, DELETE...
+    $request_uri = $_SERVER['REQUEST_URI']; // /example?foo=bar1&foo=bar2 => /example?foo=bar2
 
+    function get_page_content() {
+        return '';
+    }
 
 ?>
