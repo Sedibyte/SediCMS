9c1a6fe017420992cfe80ecad17d5053e4feb1b1
Tyler Hanavan <tyler.hanavan@sedibyte.com>
2024-03-16 16:26:35 -0400

    add query param parsing

--- src/index.php
+++ src/index.php
@@ -2,6 +2,12 @@
 
     $request_method = $_SERVER['REQUEST_METHOD']; // GET, POST, PUT, DELETE...
     $request_uri = $_SERVER['REQUEST_URI']; // /example?foo=bar1&foo=bar2 => /example?foo=bar2
+    $request_is_https = isset($_SERVER['HTTPS'] && $_SERVER['HTTPS'] !== 'off'); // false
+
+    $parsed_uri = parse_url($request_uri, PHP_URL_PATH); // /example
+    $parsed_uri_parameters = explode('?', $request_uri);
+
+    $parsed_uri_parameters = empty($parsed_uri_parameters) ? $parsed_uri_parameters : $parsed_uri_parameters[1];
 
     function get_page_content() {
         return '';
